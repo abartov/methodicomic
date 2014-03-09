d6d150dcaf1dc7cee3a2911e4788b554dbe4e52e
Asaf Bartov <asaf.bartov@gmail.com>
2014-03-08 20:10:17 -0800

    implemented series<->issues relationship

--- app/models/gcd/gcd_issue.rb
+++ app/models/gcd/gcd_issue.rb
@@ -7,6 +7,6 @@ module GCD
     if ActiveRecord::VERSION::STRING < '4.0.0' || defined?(ProtectedAttributes)
       attr_accessible :number, :volume, :no_volume, :display_volume_with_number, :series_id, :indicia_publisher_id, :indicia_pub_not_printed, :brand_id, :no_brand, :publication_date, :key_date, :sort_code, :price, :page_count, :page_count_uncertain, :indicia_frequency, :no_indicia_frequency, :editing, :no_editing, :notes, :created, :modified, :reserved, :deleted, :is_indexed, :isbn, :valid_isbn, :no_isbn, :variant_of_id, :variant_name, :barcode, :no_barcode, :title, :no_title, :on_sale_date, :on_sale_date_uncertain, :rating, :no_rating
     end
-
+    belongs_to :series, class_name: 'GcdSeries', foreign_key: 'series_id', inverse_of: :issues
   end
 end
